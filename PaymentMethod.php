16b72a8b87dbdbd17a85b03292265845bf105bda
Francisco Memoli Olmos <francisco.memoli@gmail.com>
2015-12-16 08:51:35 -0300

    New entity Payment method

---
+++ PaymentMethod.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Flower\ModelBundle\Entity\Sales;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PaymentMethod
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Flower\ModelBundle\Entity\Sales\PaymentMethodRepository")
+ */
+class PaymentMethod
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\Column(name="sale", type="object")
+     */
+    private $sale;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return PaymentMethod
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set sale
+     *
+     * @param \stdClass $sale
+     * @return PaymentMethod
+     */
+    public function setSale($sale)
+    {
+        $this->sale = $sale;
+
+        return $this;
+    }
+
+    /**
+     * Get sale
+     *
+     * @return \stdClass 
+     */
+    public function getSale()
+    {
+        return $this->sale;
+    }
+}
