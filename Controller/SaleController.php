a2c6626690e1ed977f5ce2d459b1e68e48ac3969
Juan Manuel Aguero <juanma.aguero@gmail>
2016-02-29 14:11:57 -0300

    Add translation and use of consigured organization info

--- Controller/SaleController.php
+++ Controller/SaleController.php
@@ -11,6 +11,7 @@ use Flower\ModelBundle\Entity\Sales\Sale;
 use Flower\SalesBundle\Form\Type\SaleType;
 use Doctrine\ORM\QueryBuilder;
 use Ps\PdfBundle\Annotation\Pdf;
+
 /**
  * Sale controller.
  *
@@ -31,8 +32,8 @@ class SaleController extends BaseController
         $saleAlias = "s";
         $accountAlias = "a";
         $qb = $em->getRepository('FlowerModelBundle:Sales\Sale')->createQueryBuilder($saleAlias);
-        $qb->leftJoin("s.account",$accountAlias);
-        $qb->leftJoin("s.status","es");
+        $qb->leftJoin("s.account", $accountAlias);
+        $qb->leftJoin("s.status", "es");
         /* filter by org security groups */
         if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
             $secGroupSrv = $this->get('user.service.securitygroup');
@@ -41,18 +42,18 @@ class SaleController extends BaseController
         $translator = $this->get('translator');
         $dateformat = $translator->trans('fullDateTime');
         $filters = array('accountFilter' => "a.id",
-                         'ownerFilter' => 's.owner',
-                         'statusFilter' => 'es.id',
-                         'startDateFilter' => array("field"=> "s.created", "type" => "date", "format" => $dateformat, "operation" => ">"),
-                         'endDateFilter' => array("field"=> "s.created", "type" => "date","format" => $dateformat , "operation" => "<="));
+            'ownerFilter' => 's.owner',
+            'statusFilter' => 'es.id',
+            'startDateFilter' => array("field" => "s.created", "type" => "date", "format" => $dateformat, "operation" => ">"),
+            'endDateFilter' => array("field" => "s.created", "type" => "date", "format" => $dateformat, "operation" => "<="));
 
-        if($request->query->has('reset')) {
+        if ($request->query->has('reset')) {
             $request->getSession()->set('filter.sale', null);
             return $this->redirectToRoute("sale");
         }
 
-        $this->saveFilters($request, $filters, 'sale','sale');
-        $paginator = $this->filter($qb,'sale',$request);
+        $this->saveFilters($request, $filters, 'sale', 'sale');
+        $paginator = $this->filter($qb, 'sale', $request);
 
 
         $accounts = $this->get("client.service.account")->findAll();
@@ -61,20 +62,20 @@ class SaleController extends BaseController
         $users = $em->getRepository('FlowerModelBundle:User\User')->findAll();
         $accountFilter = $request->query->get("accountFilter");
         $filters = $this->getFilters('sale');
-        if(!$accountFilter && $filters['accountFilter'] && $filters['accountFilter']["value"]){
+        if (!$accountFilter && $filters['accountFilter'] && $filters['accountFilter']["value"]) {
             $accountFilter = $filters['accountFilter']["value"];
         }
         $ownerFilter = $request->query->get("ownerFilter");
-        if(!$ownerFilter && $filters['ownerFilter'] && $filters['ownerFilter']["value"]){
+        if (!$ownerFilter && $filters['ownerFilter'] && $filters['ownerFilter']["value"]) {
             $ownerFilter = $filters['ownerFilter']["value"];
         }
         $statusFilter = $request->query->get("statusFilter");
-        if(!$statusFilter && $filters['statusFilter'] && $filters['statusFilter']["value"]){
+        if (!$statusFilter && $filters['statusFilter'] && $filters['statusFilter']["value"]) {
             $statusFilter = $filters['statusFilter']["value"];
-        }    
+        }
         return array(
-            'startDateFilter' => isset($filters['startDateFilter'])?$filters['startDateFilter']["value"] : null,
-            'endDateFilter' => isset($filters['endDateFilter'])?$filters['endDateFilter']["value"] : null,
+            'startDateFilter' => isset($filters['startDateFilter']) ? $filters['startDateFilter']["value"] : null,
+            'endDateFilter' => isset($filters['endDateFilter']) ? $filters['endDateFilter']["value"] : null,
             'users' => $users,
             'statuses' => $status,
             'ownerFilter' => $ownerFilter,
@@ -85,7 +86,7 @@ class SaleController extends BaseController
         );
     }
 
-        /**
+    /**
      *
      * @Route("/export", name="sale_export")
      * @Method("GET")
@@ -94,18 +95,18 @@ class SaleController extends BaseController
     {
         $em = $this->getDoctrine()->getManager();
         $qb = $em->getRepository('FlowerModelBundle:Sales\Sale')->createQueryBuilder('s');
-        $qb->leftJoin("s.account","a");
+        $qb->leftJoin("s.account", "a");
 
         $limit = 20;
         $currPage = $request->query->get('page');
-        if($currPage){
-            $sales = $this->filter($qb,'sale',$request, $limit, $currPage);
+        if ($currPage) {
+            $sales = $this->filter($qb, 'sale', $request, $limit, $currPage);
         } else {
-            $sales = $this->filter($qb,'sale',$request, -1);
+            $sales = $this->filter($qb, 'sale', $request, -1);
         }
-        
+
         $data = $this->get("sales.service.sale")->saleDataExport($sales);
-        $this->get("sales.service.excelexport")->exportData($data,"Ventas","Exportación de ventas.");
+        $this->get("sales.service.excelexport")->exportData($data, "Ventas", "Exportación de ventas.");
         die();
         return $this->redirectToRoute("sale");
     }
@@ -136,7 +137,7 @@ class SaleController extends BaseController
     public function showAction(Sale $sale)
     {
         return array(
-        'sale' => $sale,
+            'sale' => $sale,
         );
     }
 
@@ -154,17 +155,17 @@ class SaleController extends BaseController
     }
 
     /**
-    * @Pdf()
-    * @Route("/print/{id}/pdf", name="sale_pdf_export", requirements={"id"="\d+"},  defaults={ "_format"="pdf" })
-    * @Template()
-    */
-        public function printPDFAction(Sale $sale)
-        {
-            $em = $this->getDoctrine()->getManager();
-            $paymentMethods = $em->getRepository('FlowerModelBundle:Sales\PaymentMethod')->findAll();
-            return array(
+     * @Pdf()
+     * @Route("/print/{id}/pdf", name="sale_pdf_export", requirements={"id"="\d+"},  defaults={ "_format"="pdf" })
+     * @Template()
+     */
+    public function printPDFAction(Sale $sale)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $paymentMethods = $em->getRepository('FlowerModelBundle:Sales\PaymentMethod')->findAll();
+        return array(
             'sale' => $sale,
             'paymentMethods' => $paymentMethods,
-            );
-        }
+        );
+    }
 }
--- Service/SaleService.php
+++ Service/SaleService.php
@@ -4,6 +4,7 @@ namespace Flower\SalesBundle\Service;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use PHPExcel_Style_Fill;
+
 /**
  * Description of AccountService
  *
@@ -30,8 +31,8 @@ class SaleService
     {
         $data = array();
         $data["header"] =
-        array("values" => 
-                 array( 
+            array("values" =>
+                array(
                     "Numero de Venta",
                     "Cuenta",
                     "Creado Por",
@@ -51,67 +52,67 @@ class SaleService
                     'Observaciones',
                     'Creado',
                     'Actualizado',),
-            "styles" =>  array(
-                                'fill' => array(
-                                    'type' => PHPExcel_Style_Fill::FILL_SOLID,
-                                    'color' => array('rgb' => 'd22729')
-                                )
-                            ));
-        $subHeader = array("values" => 
-                        array( 
-                            " ",
-                            "Producto",
-                            "Unidades",
-                            "Precio x Unidad",
-                            "Total"));
+                "styles" => array(
+                    'fill' => array(
+                        'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                        'color' => array('rgb' => 'd22729')
+                    )
+                ));
+        $subHeader = array("values" =>
+            array(
+                " ",
+                "Producto",
+                "Unidades",
+                "Precio x Unidad",
+                "Total"));
         $index = 1;
         foreach ($sales as $sale) {
-            $id = $sale->getId()? : " ";
-            $account = $sale->getAccount()? : " ";
-            $owner = $sale->getOwner()? $sale->getOwner()->getHappyName(): " ";
-            $total = $sale->getTotal()? : " ";
-            $tax = $sale->getTax()? : " ";
-            $totalWithTax = $sale->getTotalWithTax()? : " ";
-            $paymentmethod = $sale->getPaymentMethod()? : " ";
-            $street = $sale->getStreet()? : " ";
-            $streetNumber = $sale->getStreetNumber()? : " ";
-            $department = $sale->getDepartment()? : " ";
-            $locality = $sale->getLocality()? : " ";
-            $zipCode = $sale->getZipCode()? : " ";
-            $city = $sale->getCity()? : " ";
+            $id = $sale->getId() ?: " ";
+            $account = $sale->getAccount() ?: " ";
+            $owner = $sale->getOwner() ? $sale->getOwner()->getHappyName() : " ";
+            $total = $sale->getTotal() ?: " ";
+            $tax = $sale->getTax() ?: " ";
+            $totalWithTax = $sale->getTotalWithTax() ?: " ";
+            $paymentmethod = $sale->getPaymentMethod() ?: " ";
+            $street = $sale->getStreet() ?: " ";
+            $streetNumber = $sale->getStreetNumber() ?: " ";
+            $department = $sale->getDepartment() ?: " ";
+            $locality = $sale->getLocality() ?: " ";
+            $zipCode = $sale->getZipCode() ?: " ";
+            $city = $sale->getCity() ?: " ";
 
 
-            $email  = ($sale->getContact() && $sale->getContact()->getEmail())? $sale->getContact()->getEmail() : " ";
-            $phone  = ($sale->getContact() && $sale->getContact()->getPhone())? $sale->getContact()->getPhone() : " ";
-            $nombre  = ($sale->getContact() && $sale->getContact()->getHappyName())? $sale->getContact()->getHappyName() : " ";
-            $observations = $sale->getObservations()? : " ";
-            $created = $sale->getCreated()? $sale->getCreated()->format("d/m/y H:i"): " ";
-            $updated = $sale->getUpdated()? $sale->getUpdated()->format("d/m/y H:i"): " ";
+            $email = ($sale->getContact() && $sale->getContact()->getEmail()) ? $sale->getContact()->getEmail() : " ";
+            $phone = ($sale->getContact() && $sale->getContact()->getPhone()) ? $sale->getContact()->getPhone() : " ";
+            $nombre = ($sale->getContact() && $sale->getContact()->getHappyName()) ? $sale->getContact()->getHappyName() : " ";
+            $observations = $sale->getObservations() ?: " ";
+            $created = $sale->getCreated() ? $sale->getCreated()->format("d/m/y H:i") : " ";
+            $updated = $sale->getUpdated() ? $sale->getUpdated()->format("d/m/y H:i") : " ";
 
-            $data[$index++] = 
-            array("values" => 
-                array(
-                    $id,
-                    $account ,
-                    $owner,
-                    $total,
-                    $tax,
-                    $totalWithTax,
-                    $paymentmethod,
-                    $street,
-                    $streetNumber,
-                    $department,
-                    $locality,
-                    $zipCode,
-                    $city,
-                    $email,
-                    $phone,
-                    $nombre,
-                    $observations,
-                    $created,
-                    $updated
+            $data[$index++] =
+                array("values" =>
+                    array(
+                        $id,
+                        $account,
+                        $owner,
+                        $total,
+                        $tax,
+                        $totalWithTax,
+                        $paymentmethod,
+                        $street,
+                        $streetNumber,
+                        $department,
+                        $locality,
+                        $zipCode,
+                        $city,
+                        $email,
+                        $phone,
+                        $nombre,
+                        $observations,
+                        $created,
+                        $updated
                     ),
-                    "styles" =>  array(
+                    "styles" => array(
                         'fill' => array(
                             'type' => PHPExcel_Style_Fill::FILL_SOLID,
                             'color' => array('rgb' => '929292')
@@ -119,22 +120,22 @@ class SaleService
                     ));
             $data[$index++] = $subHeader;
             foreach ($sale->getSaleItems() as $item) {
-                
-                $units = $item->getUnits()? : " ";
-                $unitPrice = $item->getUnitPrice()? : " ";
-                $product = $item->getProduct()? : " ";
-                $total = $item->getTotal()? : " ";
-                $data[$index++] = 
-                    array("values" => 
+
+                $units = $item->getUnits() ?: " ";
+                $unitPrice = $item->getUnitPrice() ?: " ";
+                $product = $item->getProduct() ?: " ";
+                $total = $item->getTotal() ?: " ";
+                $data[$index++] =
+                    array("values" =>
                         array(
                             " ",
                             $product,
                             $units,
                             $unitPrice,
                             $total
-                            ));
+                        ));
             }
-            
+
         }
         return $data;
     }
